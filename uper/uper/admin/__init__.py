2a04c18dce5d88b3845589d8cad3e234798c80bb
scil <scil.zc@gmail.com>
2019-03-31 09:33:04 +0800

    init

---
+++ uper/uper/admin/__init__.py
@@ -0,0 +1,8 @@
+# This repository is a good example of how flask-login and flask-security can be connected with flask-admin.
+#   https://github.com/briancappello/flask-react-spa
+# In this reddit post the author of the boilerplate describes shortly the implementation logic.
+#   https://www.reddit.com/r/flask/comments/7tr82o/example_integrating_flask_admin_into_flask_webapp/
+# https://stackoverflow.com/questions/11804922/flask-admin-flask-login-and-or-flask-principal?rq=1
+
+from .macro import macro
+from .model_admin import ModelAdmin
